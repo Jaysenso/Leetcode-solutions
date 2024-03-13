55557e5f9c48392053079e591b040208c27609fc
J'sen Ong <jsenongxuan@gmail.com>
2024-03-13 12:16:01 +0800

    Create 1200_Minimum_Absolute_Difference.js

---
+++ Easy/1200_Minimum_Absolute_Difference.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+
+ /**
+
+Iterate through arr, let difference = INT_MAX; and sort the array in ascending (is easier to iterate through & compute abs. difference and the resultant array has to be in ascending order)
+compute the difference between current element and the element after current element
+we check if the computed difference is less than difference variable : 
+if yes => update difference with its value and reinitialize the result array and push these 2 elements
+if the differences are equal => we dont reinitialize, but just append it into the res array
+if no => we dont do anything
+
+*/
+var minimumAbsDifference = function(arr) {
+    let minDiff = Number.MAX_VALUE;
+    let res = [];
+    arr.sort((a,b) => a - b);
+
+    for(let i = 0; i < arr.length; i++){
+
+        let diff = arr[i+1] - arr[i];
+
+        if(minDiff > diff) {
+            minDiff = diff;
+            res = [[arr[i], arr[i+1]]];
+        }else if(minDiff === diff)
+            res.push([arr[i],arr[i + 1]]);
+    }   
+        
+    return res;
+};
