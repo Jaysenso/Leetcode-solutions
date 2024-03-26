45f357533e19dfe71f5b404d15aca372c8ed547a
J'sen Ong <jsenongxuan@gmail.com>
2024-03-27 00:53:46 +0800

    Create 2390_Removing_Stars_From_a_String.js

---
+++ LeetCode 75 Study Plan/2390_Removing_Stars_From_a_String.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+    const letters = [];
+    for(const char of s) 
+      char === "*" ? letters.pop() : letters.push(char); 
+    return letters.join("");
+};
+
